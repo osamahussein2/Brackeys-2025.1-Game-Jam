a369e0013faf098189b273328c73996bda8f1c57
osamahussein2 <osamahusseinn2@gmail.com>
2025-02-18 13:55:28 -0500

    Currently held weapon shows up in player's HUD

--- Assets/Scripts/swap_items.cs
+++ Assets/Scripts/swap_items.cs
@@ -1,50 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class swap_items : MonoBehaviour
 {
     public int curr_item;
+
+    [SerializeField] private Image weaponImage;
+
+    public List<Sprite> weapons;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        curr_item = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        weaponImage.sprite = weapons[curr_item];
+
         if (Input.GetKey("1"))
         {
-            curr_item = 1;
+            curr_item = 0;
         }
         if (Input.GetKey("2"))
         {
-            curr_item = 2;
+            curr_item = 1;
         }
         if (Input.GetKey("3"))
         {
-            curr_item = 3;
+            curr_item = 2;
         }
         if (Input.GetKey("4"))
         {
-            curr_item = 4;
+            curr_item = 3;
         }
         if (Input.GetKey("5"))
         {
-            curr_item = 5;
+            curr_item = 4;
         }
         if (Input.GetKey("6"))
         {
-            curr_item = 6;
+            curr_item = 5;
         }
         if (Input.GetKey("7"))
         {
-            curr_item = 7;
+            curr_item = 6;
         }
         if (Input.GetKey("8"))
         {
-            curr_item = 8;
+            curr_item = 7;
         }
     }
 }
