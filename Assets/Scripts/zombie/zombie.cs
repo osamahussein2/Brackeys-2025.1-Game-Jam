825b4bbf7b403ed407eb80f48169ef0d048a8f02
Osama Hussein <osamahusseinn2@gmail.com>
2025-02-17 22:15:56 -0500

    Zombie animations implemented

--- Assets/Scripts/zombie/zombie.cs
+++ Assets/Scripts/zombie/zombie.cs
@@ -15,6 +15,10 @@ public class zombie : MonoBehaviour, IDamagable
     public GameObject player;
     Vector3 randPos;
     public float speed = 0.75f;
+
+    private Animator zombieAnimator;
+    private SpriteRenderer zombieSprite;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -24,6 +28,9 @@ public class zombie : MonoBehaviour, IDamagable
         zombieGen.count += 1;
         randPos = transform.position;
         player = GameObject.Find("Player");
+
+        zombieAnimator = GetComponent<Animator>();
+        zombieSprite = GetComponent<SpriteRenderer>();
     }
     private void OnDestroy() {
         zombieGen.count -= 1;
@@ -39,8 +46,48 @@ public class zombie : MonoBehaviour, IDamagable
             timer = 0f;
         }
         Vector3 direction = randPos - transform.position;
-        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+        //transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
         transform.position = Vector3.MoveTowards(transform.position, randPos, speed);
+
+        if (direction.x <= -0.01f)
+        {
+            zombieAnimator.SetBool("IsMovingLeft", true);
+            zombieAnimator.SetBool("IsMovingRight", false);
+
+            zombieSprite.flipX = true;
+        }
+
+        else if (direction.x >= 0.01f)
+        {
+            zombieAnimator.SetBool("IsMovingRight", true);
+            zombieAnimator.SetBool("IsMovingLeft", false);
+
+            zombieSprite.flipX = false;
+        }
+
+        if (direction.y <= -0.01f)
+        {
+            zombieAnimator.SetBool("IsMovingDown", true);
+            zombieAnimator.SetBool("IsMovingUp", false);
+
+            zombieSprite.flipY = false;
+        }
+
+        else if (direction.y >= 0.01f)
+        {
+            zombieAnimator.SetBool("IsMovingUp", true);
+            zombieAnimator.SetBool("IsMovingDown", false);
+
+            zombieSprite.flipY = false;
+        }
+
+        if (direction == Vector3.zero)
+        {
+            zombieAnimator.SetBool("IsMovingLeft", false);
+            zombieAnimator.SetBool("IsMovingRight", false);
+            zombieAnimator.SetBool("IsMovingUp", false);
+            zombieAnimator.SetBool("IsMovingDown", false);
+        }
     }
     float dist(Vector3 a, Vector3 b)
     {
@@ -55,8 +102,48 @@ public class zombie : MonoBehaviour, IDamagable
         if (seesPlayer)
         {
             Vector3 direction = player.transform.position - transform.position;
-            transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+            //transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+
+            if (direction.x <= -0.01f)
+            {
+                zombieAnimator.SetBool("IsMovingLeft", true);
+                zombieAnimator.SetBool("IsMovingRight", false);
+
+                zombieSprite.flipX = true;
+            }
+
+            else if (direction.x >= 0.01f)
+            {
+                zombieAnimator.SetBool("IsMovingRight", true);
+                zombieAnimator.SetBool("IsMovingLeft", false);
+
+                zombieSprite.flipX = false;
+            }
+
+            if (direction.y <= -0.01f)
+            {
+                zombieAnimator.SetBool("IsMovingDown", true);
+                zombieAnimator.SetBool("IsMovingUp", false);
+
+                zombieSprite.flipY = false;
+            }
+
+            else if (direction.y >= 0.01f)
+            {
+                zombieAnimator.SetBool("IsMovingUp", true);
+                zombieAnimator.SetBool("IsMovingDown", false);
+
+                zombieSprite.flipY = false;
+            }
+
+            if (direction == Vector3.zero)
+            {
+                zombieAnimator.SetBool("IsMovingLeft", false);
+                zombieAnimator.SetBool("IsMovingRight", false);
+                zombieAnimator.SetBool("IsMovingUp", false);
+                zombieAnimator.SetBool("IsMovingDown", false);
+            }
         }
         Vector3 right = Quaternion.Euler(0, 0, -coneSize) * transform.up;
         Vector3 left = Quaternion.Euler(0, 0, coneSize) * transform.up;
