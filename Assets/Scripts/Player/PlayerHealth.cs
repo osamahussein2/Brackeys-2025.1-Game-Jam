ef32408519bb9c8decf83b8fd3b3fd8f5f78d195
Osama Hussein <osamahusseinn2@gmail.com>
2025-02-17 19:54:01 -0500

    Zombies can damage the player

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth :MonoBehaviour,  IDamagable
 {
     private float playerHealth = 100f;
-    
+    private float houseHealth = 100f;
+
+    [SerializeField] private Slider playerHP;
+    [SerializeField] private Slider houseHP;
+
     public static PlayerHealth Instance {  get; private set; }
 
     private void Awake()
     {
         Instance = this;
+
+        // Start the player and house health to their max health
+        playerHealth = playerHP.maxValue;
+        houseHealth = houseHP.maxValue;
+
+        // Set the player and house HP slider values equal to their initialized player and house health, respectively
+        playerHP.value = playerHealth;
+        houseHP.value = houseHealth;
+    }
+
+    void Update()
+    {
+        // Update the player and house HP slider values based on player and house health, respectively
+        playerHP.value = playerHealth;
+        houseHP.value = houseHealth;
     }
 
     public void Damage(float damageAmount)
@@ -22,5 +42,22 @@ public class PlayerHealth :MonoBehaviour,  IDamagable
     private void Die()
     { 
         //Game over logic
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Damage the player
+            Damage(1f * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Damage(0f);
+        }
     }
 }
